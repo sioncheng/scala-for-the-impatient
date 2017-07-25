0e96e86977c799c0ccdc1a87790b362c0f82b951
程勇桥 <cyq@dst64953.cn1.global.ctrip.com>
2017-07-25 20:44:43 +0800

    first chapter

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "scala-for-the-impatient"
+
+version := "0.1"
+
+resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+scalaVersion := "2.12.0"
+
---
+++ src/main/scala/ch1/MainApp.scala
@@ -0,0 +1,27 @@
+package ch1
+
+/**
+  * Created by cyq on 25/07/2017.
+  */
+object MainApp extends App {
+
+    println(1.to(10)) //Range 1 to 10
+    println("hello".intersect("world")) // lo
+
+    val ten2twenty = 10 to 20
+    println(ten2twenty)
+
+    import scala.math._
+    println(sqrt(10))
+    println(min(3, 10))
+
+    println(BigInt("12342424"))
+    println("hello"(1))
+    println(BigDecimal.apply("10.0"))
+
+    val what = "what are you doing"
+    println(what.count(x => x.compareTo('d') > 0))
+    println(what.map(x => x.toUpper))
+    println(what.containsSlice('a'.to('d')))
+    println(what.containsSlice('w'.to('w')))
+}
